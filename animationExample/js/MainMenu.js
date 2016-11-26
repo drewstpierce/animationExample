514f14ca7f8cb5ac284d079a874b58074275a258
drewstpierce <bicandvlood@gmail.com>
2016-11-25 22:27:51 -0500

    Update MainMenu.js

--- animationExample/js/MainMenu.js
+++ animationExample/js/MainMenu.js
@@ -4,9 +4,7 @@ Game.MainMenu = function (game) {
 
 Game.MainMenu.prototype = {
     create: function () {
-        // create main menu text and images -
-        // create a "Start Game" mechanism - variety of ways to do this...
-        
+    
         this.loadingText = this.add.text(this.game.width/2, this.game.height / 2, "Press Enter to start", { font: "10px monospace", fill: "#fff" });
         this.loadingText.anchor.setTo(0.5, 0.5);
 
